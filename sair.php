2abd78a6635d6201e72f4b9b8be419cebe806f36
Pascoal Nzola Tondo <139359421+pascoalnzola@users.noreply.github.com>
2024-09-02 08:03:38 +0100

    matando a sessão
    funcao para terminar a sessão

--- sair.php
+++ sair.php
@@ -1,4 +1,7 @@
 <?php
-    session_unset();
+    include("Banco_dados/config.php");
+    unset($_SESSION['user_id']);
+    unset($_SESSION['usuario']);
+    session_destroy();
     header("Location: login.php");
 ?>
